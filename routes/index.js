000ff2d58051bb86d610822db86532b25985ffb8
Justin Wilkey <justin.wilkey@tentkeep.com>
2018-01-13 16:13:16 -0500

    Add mailgun

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Mailgun = require('mailgun-js')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Tentkeep server' })
+})
+
+router.post('/mail', (req, res) => {
+  const apiKey = process.env.MAILGUN_KEY
+  const domain = process.env.MAILGUN_DOMAIN
+  var mailgun = new Mailgun({apiKey, domain})
+
+  var data = {
+    from: process.env.MAILGUN_FROM,
+    to: process.env.MAILGUN_TO,
+    subject: req.body.subject,
+    html: req.body.content
+  }
+
+  mailgun.messages().send(data, function (err, body) {
+    if (err) {
+      console.log(err)
+      throw new Error('Error sending message')
+    } else {
+      res.json({success: true})
+    }
+  })
+})
 
 module.exports = router;
